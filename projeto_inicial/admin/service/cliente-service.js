9bd5789b3423b95cd4d38fa44232a7d7806700b1
Dan <danilobomfim.si@gmail.com>
2021-12-11 16:03:38 -0300

    Dev(1)

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,10 +1,61 @@
-const http = new XMLHttpRequest();
+const listaClientes = () => {
+    return fetch(`http://localhost:3000/profile`)
+        .then(resposta => {
 
-http.open("GET", "http://localhost:3000/profile");
+            return resposta.json()
 
-http.send();
+        })
+}
 
-http.onload = () => {
-  const data = http.response;
-  console.log(data);
-};
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+        .then(resposta => {
+            return resposta.body
+        })
+}
+
+const removeCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    })
+}
+
+const detalhaCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`)
+        .then(resposta => {
+            return resposta.json()
+        })
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+        .then(resposta => {
+            return resposta.json()
+        })
+}
+
+export const clienteService = {
+    listaClientes,
+    criaCliente,
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
+}
